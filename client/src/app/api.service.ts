77e1a3245f2e6fdeea640e461a3c6324d22551a0
Adit Modi <adit.modi24@gmail.com>
2020-07-29 17:38:34 +0530

    Update api.service.ts

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class APIService {
 
-  ENDPOINT = '<endpoint>';
+  ENDPOINT = '<Endpoint>';
 
   constructor(private http:HttpClient) {}
 
